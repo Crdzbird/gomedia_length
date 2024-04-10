2137cb1efa1229ee78558537b411cf94901975d0
Crdzbird <luisalfonsocb83@gmail.com>
2024-04-10 16:27:00 +0200

    Minor change on declaration

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ package gomedia_length
 // #include "media_library/library.h"
 import "C"
 
-func getVideoDuration(path string) float64 {
+func GetVideoDuration(path string) float64 {
 	return float64(C.getVideoDuration(C.CString(path)))
 }
 
-func getAudioDuration(path string) float64 {
+func GetAudioDuration(path string) float64 {
 	return float64(C.getAudioDuration(C.CString(path)))
 }
 
-func isValidMediaFile(path string) bool {
+func IsValidMediaFile(path string) bool {
 	return C.isValidMediaFile(C.CString(path)) == 1
 }
--- main_test.go
+++ main_test.go
@@ -19,28 +19,28 @@ func TestGetVideoDurationWithInvalidPath(t *testing.T) {
 }
 
 func TestGetAudioDurationWithValidPath(t *testing.T) {
-	duration := getAudioDuration("/Users/crdzbird/Desktop/qwerty.mp3")
+	duration := GetAudioDuration("/Users/crdzbird/Desktop/qwerty.mp3")
 	if duration <= 0 {
 		t.Errorf("Expected duration to be greater than 0, got %f", duration)
 	}
 }
 
 func TestGetAudioDurationWithInvalidPath(t *testing.T) {
-	duration := getAudioDuration("/Users/crdzbird/Desktop/noMusic.mp3")
+	duration := GetAudioDuration("/Users/crdzbird/Desktop/noMusic.mp3")
 	if duration != 0 {
 		t.Errorf("Expected duration to be 0 for invalid path, got %f", duration)
 	}
 }
 
 func TestIsValidMediaFileWithValidPath(t *testing.T) {
-	isValid := isValidMediaFile("/Users/crdzbird/Desktop/peruano.mp4")
+	isValid := IsValidMediaFile("/Users/crdzbird/Desktop/peruano.mp4")
 	if !isValid {
 		t.Errorf("Expected file to be valid, got %v", isValid)
 	}
 }
 
 func TestIsValidMediaFileWithInvalidPath(t *testing.T) {
-	isValid := isValidMediaFile("/Users/crdzbird/Desktop/inexistent.mp4")
+	isValid := IsValidMediaFile("/Users/crdzbird/Desktop/inexistent.mp4")
 	if isValid {
 		t.Errorf("Expected file to be invalid, got %v", isValid)
 	}
